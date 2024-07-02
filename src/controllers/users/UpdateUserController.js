253916910da055b6578c3eb0ce9f44e5792ceb2f
carlos miguel <carlosmiguelcm820@gmail.com>
2024-07-02 13:35:16 -0400

    update validator's

--- src/controllers/users/UpdateUserController.js
+++ src/controllers/users/UpdateUserController.js
@@ -9,30 +9,35 @@ export class UpdateUserController {
             const id = req.params.id
 
             //// VALIDATORS
-            if (Validator.validateVazio(body.email)
-                || Validator.validateVazio(body.phone)
-                || Validator.validateVazio(body.dateBirth)
-                || Validator.validateVazio(body.password)
-                || Validator.validateVazio(body.fullName)
-                || Validator.validateVazio(body.adress)
-                || Validator.validateVazio(body.level)) {
-                return res.status(400).json({ message: "Algum campo está vazio!" })
-            }
+            if (Validator.validateVazio(body.password)) {
+                if (Validator.validateVazio(body.email)
+                    || Validator.validateVazio(body.phone)
+                    || Validator.validateVazio(body.dateBirth)
+                    || Validator.validateVazio(body.fullName)
+                    || Validator.validateVazio(body.adress)
+                    || Validator.validateVazio(body.level)) {
+                    return res.status(400).json({ message: "Algum campo está vazio!" })
+                }
 
-            if (!Validator.validateEmail(body.email)) {
-                return res.status(400).json({ message: "EMAIL inválido!" })
-            }
+                if (!Validator.validateEmail(body.email)) {
+                    return res.status(400).json({ message: "EMAIL inválido!" })
+                }
 
-            if (!Validator.validatePhoneNumber(body.phone)) {
-                return res.status(400).json({ message: "TELEFONE inválido!" })
-            }
+                if (!Validator.validatePhoneNumber(body.phone)) {
+                    return res.status(400).json({ message: "TELEFONE inválido!" })
+                }
 
-            if (!Validator.validatePassword(body.password)) {
-                return res.status(400).json({ message: "SENHA inválida!" })
-            }
+                if (!Validator.validateData(body.dateBirth)) {
+                    return res.status(400).json({ message: "DATA DE NASCIMENTO inválida!" })
+                }
+            } else {
+                if (!Validator.validatePassword(body.password)) {
+                    return res.status(400).json({ message: "SENHA inválida!" })
+                }
 
-            if (!Validator.validateData(body.dateBirth)) {
-                return res.status(400).json({ message: "DATA DE NASCIMENTO inválida!" })
+                const salt = 12;
+                const hashPassword = await bcrypt.hash(body.senha, salt);
+                body.senha = hashPassword;  // Atualiza o campo senha com o hash
             }
             //// VALIDATORS
 
