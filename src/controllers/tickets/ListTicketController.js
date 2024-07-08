8d483764424a92858ff1529a56e716466c3920dd
Carlos <carlosmiguelcm820@gmail.com>
2024-07-08 10:48:19 -0400

    edit list ticket

--- src/controllers/tickets/ListTicketController.js
+++ src/controllers/tickets/ListTicketController.js
@@ -4,10 +4,13 @@ import { TicketSchema } from "../../schema/ticktets.js";
 export class ListTicketController {
     async list(req, res) {
         try {
+            const queryParams = req.query
             const ticketRepository = AppDataSource.getRepository(TicketSchema)
             const ticket = await ticketRepository.createQueryBuilder('ticket')
             .leftJoinAndSelect("ticket.status", "status")
             .leftJoinAndSelect("ticket.category", "categories")
+            .leftJoinAndSelect("ticket.user", "users")
+            .where(queryParams)
             .getMany()
             return res.json(ticket)
         } catch (error) {
