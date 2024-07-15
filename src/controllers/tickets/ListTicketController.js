7ece7a8ec205663de0f99ab0dae0ed7edfe34d6d
carlos miguel <carlosmiguelcm820@gmail.com>
2024-07-15 14:38:34 -0400

    update list ticket controller

--- src/controllers/tickets/ListTicketController.js
+++ src/controllers/tickets/ListTicketController.js
@@ -4,22 +4,26 @@ import { TicketSchema } from "../../schema/ticktets.js";
 export class ListTicketController {
     async list(req, res) {
         try {
-            const { search } = req.query
-            const queryParams = req.query
+            const { search, ...queryParams } = req.query
             const ticketRepository = AppDataSource.getRepository(TicketSchema)
             let queryBuilder = ticketRepository.createQueryBuilder('ticket')
                 .leftJoinAndSelect("ticket.status", "status")
                 .leftJoinAndSelect("ticket.category", "categories")
                 .leftJoinAndSelect("ticket.user", "users")
                 .orderBy('ticket.id', 'DESC')
-            if (search) {
+            if (search && search !== "" && search !== null) {
                 queryBuilder = queryBuilder.where(
-                    "ticket.title LIKE :search OR ticket.body LIKE :search OR status.status LIKE :search OR categories.category LIKE :search",
+                    `ticket.title LIKE :search OR ticket.body LIKE :search OR status.status LIKE :search OR categories.category LIKE :search`,
                     { search: `%${search}%` }
-                )
-            } else {
-                queryBuilder = queryBuilder.where(queryParams)
+                );
             }
+
+            for (const [key, value] of Object.entries(queryParams)) {
+                if (value !== undefined && value !== null && value !== '') {
+                    queryBuilder = queryBuilder.andWhere(`ticket.${key} = :${key}`, { [key]: value });
+                }
+            }
+
             const ticket = await queryBuilder.getMany()
             return res.json(ticket)
         } catch (error) {
